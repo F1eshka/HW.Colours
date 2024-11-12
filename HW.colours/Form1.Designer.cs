894e39e3f6bd1e9ac41f369cca11e7bbb3ea7631
F1eshka <144143888+F1eshka@users.noreply.github.com>
2024-11-12 16:58:52 +0300

    Add files via upload

---
+++ HW.colours/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace HW.colours
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.HotTrack;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(107, 135);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 51);
+            button1.TabIndex = 0;
+            button1.Text = "Выбрать цвет";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(515, 135);
+            button2.Name = "button2";
+            button2.Size = new Size(165, 51);
+            button2.TabIndex = 1;
+            button2.Text = "Выбрать шрифт";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(262, 249);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 30);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "Какую таблетку выберете ВЫ????";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Выбор цвета и шрифта для текста";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.ForeColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (FontDialog fontDialog = new FontDialog())
+            {
+                if (fontDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Font = fontDialog.Font; 
+                }
+            }
+        }
+    }
+}
---
+++ HW.colours/Form1.cs
@@ -0,0 +1,10 @@
+namespace HW.colours
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
